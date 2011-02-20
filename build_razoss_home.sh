ec43d5b1ea443c77ddc71c41c882c09d9b736835
Daniel C <333222@gmail.com>
2011-02-20 08:56:37 +0200

    build_razoss_home.sh now call ./theosp-projects with the update-skeleton command

--- build_razoss_home.sh
+++ build_razoss_home.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./theosp-projects new --project-readable-name "Razoss" --gae-app-name "razoss-home-dev" theosp/razoss-home
+./theosp-projects update-skeleton --project-readable-name "Razoss" --gae-app-name "razoss-home-dev" theosp/razoss-home
 
 # vim:ft=bash:fdm=marker:fmr={{{,}}}:
