a4d70861db04e5a09605ce2b6961c315c5ce5d6c
Daniel C <333222@gmail.com>
2011-03-14 13:34:00 +0200

    standard api generator's api.js improved

--- api-generator-templates/standard/api.js
+++ api-generator-templates/standard/api.js
@@ -1,14 +1,25 @@
 (function ($) {
     $.{{ underscored_entity_name }}_api = {
         // list {{{
-        list: function (order, page, callback) {
+        list: function (order, page, success, error) {
             $.ajax({
                 type: 'GET',
                 cache: false,
                 url: '/{{ underscored_entity_name }}/all/',
                 data: {order_by: order, page: page},
-                success: function (result) {
-                    callback(result);
+                success: function (data, textStatus, jqXHR) {
+                    if (typeof success !== 'undefined') {
+                        success(data, textStatus, jqXHR);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (typeof error !== 'undefined') {
+                        error(jqXHR, textStatus, errorThrown);
+                    }
+
+                    if (typeof console !== 'undefined') {
+                        console.log(jqXHR, textStatus, errorThrown);
+                    }
                 },
                 dataType: 'json'
             });
@@ -16,13 +27,24 @@
         // }}}
 
         // get {{{
-        get: function (key, callback) {
+        get: function (key, success, error) {
             $.ajax({
                 type: 'GET',
                 cache: false,
                 url: '/{{ underscored_entity_name }}/' + key + '/',
-                success: function (result) {
-                    callback(result);
+                success: function (data, textStatus, jqXHR) {
+                    if (typeof success !== 'undefined') {
+                        success(data, textStatus, jqXHR);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (typeof error !== 'undefined') {
+                        error(jqXHR, textStatus, errorThrown);
+                    }
+
+                    if (typeof console !== 'undefined') {
+                        console.log(jqXHR, textStatus, errorThrown);
+                    }
                 },
                 dataType: 'json'
             });
