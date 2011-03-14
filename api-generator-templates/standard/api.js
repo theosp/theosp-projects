efef610cbaa8b8063fcfbc95f3ec84880f20c761
Daniel C <333222@gmail.com>
2011-03-14 14:49:32 +0200

    add conditional sections: allow_guests_to_get_entity, allow_guests_to_list_entities, allow_guests_to_create_entities, allow_guests_to_edit_entities

--- api-generator-templates/standard/api.js
+++ api-generator-templates/standard/api.js
@@ -1,5 +1,6 @@
 (function ($) {
     $.{{ underscored_entity_name }}_api = {
+
         // list {{{
         list: function (order, page, success, error) {
             $.ajax({
@@ -48,8 +49,65 @@
                 },
                 dataType: 'json'
             });
+        },
+        // }}}
+
+        // create {{{
+        create: function (properties, success, error) {
+            $.ajax({
+                type: 'POST',
+                cache: false,
+                url: '/{{ underscored_entity_name }}/create/',
+                data: {
+                    query: JSON.stringify(properties)
+                },
+                success: function (data, textStatus, jqXHR) {
+                    if (typeof success !== 'undefined') {
+                        success(data, textStatus, jqXHR);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (typeof error !== 'undefined') {
+                        error(jqXHR, textStatus, errorThrown);
+                    }
+
+                    if (typeof console !== 'undefined') {
+                        console.log(jqXHR, textStatus, errorThrown);
+                    }
+                },
+                dataType: 'json'
+            });
+        },
+        // }}}
+
+        // edit {{{
+        edit: function (key, properties, success, error) {
+            $.ajax({
+                type: 'POST',
+                cache: false,
+                url: '/{{ underscored_entity_name }}/' + key + '/save/',
+                data: {
+                    query: JSON.stringify(properties)
+                },
+                success: function (data, textStatus, jqXHR) {
+                    if (typeof success !== 'undefined') {
+                        success(data, textStatus, jqXHR);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (typeof error !== 'undefined') {
+                        error(jqXHR, textStatus, errorThrown);
+                    }
+
+                    if (typeof console !== 'undefined') {
+                        console.log(jqXHR, textStatus, errorThrown);
+                    }
+                },
+                dataType: 'json'
+            });
         }
         // }}}
+
     };
 })(jQuery);
 
